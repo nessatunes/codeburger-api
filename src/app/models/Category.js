12986c2670417ff6fd9ab0c70b512f537ec39592
Nessatunes <essatunes@gmail.com>
2024-05-22 15:43:25 -0300

    ajustes no caminho da imagem

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -9,7 +9,7 @@ class Category extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://localhost:3001/category-file/${this.path}`;
+            return `https://codeburger-api-production-da71.up.railway.app/category-file/${this.path}`;
           },
         },
       },
