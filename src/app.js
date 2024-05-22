153031e3efd8e3e38956a502c2022c0a2f58811c
Nessatunes <essatunes@gmail.com>
2024-05-21 22:58:04 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import { resolve } from "path";
 import cors from "cors";
 import "./database";
 
+const corsOptions = {
+  origin: 'https://codeburger-interface-chi.vercel.app/',
+  credentials: true,
+}
+
 class App {
   constructor() {
     this.app = express();
-    this.app.use(cors());
+    this.app.use(cors(corsOptions));
     this.middlewares();
     this.routes();
   }
